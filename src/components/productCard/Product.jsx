49ae6f8caf74d863ee10e64a171ed162772b709a
ArgimKaliqi <ak51029@ubt-uni.net>
2022-07-16 16:48:58 +0200

    Second Section

---
+++ src/components/productCard/Product.jsx
@@ -0,0 +1,25 @@
+import "./product.css"
+
+const Product = ({img,link}) =>{
+    return(
+        <div className="p">
+            <div className="p-browser">
+                <div className="p-circle"></div>
+                <div className="p-circle"></div>
+                <div className="p-circle"></div>
+            </div>
+            <div className="p-comp">
+                <p className="p-comp-t">E-commerce store with .Net, React,Redux, C#, Stripe,Cloudinary,TypeScript,
+                EntityFramework, CoreIdentity Authentication etc. 
+
+               </p>
+            </div>
+            <a href={link} target="_blank" rel="noreferrer">
+                <img src={img} alt="" className="p-img" />
+            </a>
+        </div>
+    )
+}
+
+
+export default Product
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5632397/pexels-photo-5632397.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://i.postimg.cc/vB5hGvPC/Capture.png",
+      link: "https://argimkaliqi.github.io/CV/",
+    },
+    {
+      id: 3,
+      img: "",
+      link: "http://lama.dev",
+    },
+  ];
